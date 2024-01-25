6ff36a97da1bcdb5ee3c2b56843be0b8a1c355b3
thowfick official <mdthowfickspeed@gmail.com>
2024-01-25 12:20:05 +0530

    init

---
+++ data/homeData/headerData.js
@@ -0,0 +1,12 @@
+const headerData = {
+    siteName: "SUNFEST",
+    navigationLinks: [
+        { label: "HOME", url: "#" },
+        { label: "SUNFEST 2018", url: "#" },
+        { label: "ARTISTS", url: "#" },
+        { label: "BLOG", url: "#" },
+        { label: "CONTACT", url: "#" }
+    ]
+};
+
+export default headerData;
